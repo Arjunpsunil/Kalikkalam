be92399887f26a238089739e376d15d7ec782e95
Arjunpsunil <arjunpsunil7@gmail.com>
2023-12-05 12:31:24 +0530

    api class

--- restapp/urls.py
+++ restapp/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-   path('getdata',views.getdata,name='getdata'),
-   path('update/<int:id>/',views.update,name='update')
+   # path('getdata',views.getdata,name='getdata'),
+   # path('update/<int:id>/',views.update,name='update'),
+   path('Studentapiview',views.Studentapiview.as_view(),name='Studentapiview'),
+   path('Studentupdate/<int:id>/',views.Studentupdate.as_view(),name='Studentupdate')
+
+
    
    
 
--- restapp/views.py
+++ restapp/views.py
@@ -42,43 +42,85 @@ from django.views.decorators.csrf import csrf_exempt
 #         return HttpResponse(status=204)
     
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
 
-@api_view(['GET','POST'])
-def getdata(request):
-    if request.method=='GET':
+# @api_view(['GET','POST'])
+# def getdata(request):
+#     if request.method=='GET':
+#         student=Student.objects.all()
+#         serializer=Studentserializer(student,many=True)
+#         return Response(serializer.data)
+#     elif request.method=='POST':
+#         serializer=Studentserializer(data=request.data)
+#         if serializer.is_valid():
+#             # serializer.save()
+#             Student.objects.create(**serializer.validated_data)
+#             return Response(serializer.data,status=201)
+#         return Response(serializer.errors,status=400)
+
+
+# @api_view(['GET','PUT','DELETE'])
+# def update(request,id):
+#     try:
+#         students=Student.objects.get(id=id)
+#     except Student.DoesNotExist:
+#         return HttpResponse(status=404)
+#     if request.method=='GET':
+#         serializer=Studentserializer(students)
+#         return Response(serializer.data)
+#     elif request.method=='PUT':
+#         serializer=Studentserializer(students,data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#              Response(serializer.errors,status=400)
+#     elif request.method=='DELETE':
+#         students.delete()
+#         return Response(status=204)
+
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+
+class Studentapiview(APIView):
+    def get(self,request):
         student=Student.objects.all()
         serializer=Studentserializer(student,many=True)
         return Response(serializer.data)
-    elif request.method=='POST':
+    def post(self,request):
         serializer=Studentserializer(data=request.data)
         if serializer.is_valid():
             # serializer.save()
             Student.objects.create(**serializer.validated_data)
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
+        
+class Studentupdate(APIView):
+    def get_obj(self, id):
+        try:
+            student = Student.objects.get(id=id)
+            return student
+        except Student.DoesNotExist:
+            return HttpResponse(status=404)
 
-
-@api_view(['GET','PUT','DELETE'])
-def update(request,id):
-    try:
-        students=Student.objects.get(id=id)
-    except Student.DoesNotExist:
-        return HttpResponse(status=404)
-    if request.method=='GET':
-        serializer=Studentserializer(students)
+    def get(self, request, id):
+        student = self.get_obj(id)
+        serializer = Studentserializer(student)
         return Response(serializer.data)
-    elif request.method=='PUT':
-        serializer=Studentserializer(students,data=request.data)
+
+    def put(self, request, id):
+        student = self.get_obj(id)
+        serializer = Studentserializer(student, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        else:
-             Response(serializer.errors,status=400)
-    elif request.method=='DELETE':
-        students.delete()
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, id):
+        student = self.get_obj(id)
+        student.delete()
         return Response(status=204)
-
-
 # Create your views here.
